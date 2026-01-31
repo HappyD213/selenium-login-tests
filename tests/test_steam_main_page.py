5f30a3f32f738a42fff785b04d9d7f002659de85
happyden <deaytthwt@gmail.com>
2026-01-31 20:27:08 +0600

    code review fixes

--- tests/test_steam_main_page.py
+++ tests/test_steam_main_page.py
@@ -1,40 +1,53 @@
-import string
-import random
+import pytest
 
+from faker import Faker
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def random_string(length=8):
-    return "".join(random.choices(string.ascii_letters, k=length))
+class DocumentReady:
+    def __call__(self, driver):
+        try:
+            return driver.execute_script("return document.readyState") == "complete"
+        except:
+            return False
 
-def test_steam_main():
-    with webdriver.Chrome() as browser:
-        url = 'https://store.steampowered.com/'
-        browser.get(url)
+class LOCATORS:
+    MAIN_PAGE_UNIQUE = (By.XPATH, "//div[contains(@class,'home_cluster_ctn') and contains(@class,'home_ctn')]")
+    MAIN_PAGE_UNIQUE2 = (By.XPATH, "//h2[@id='home_featured_and_recommended']")
+    LOGIN_BUTTON = (By.XPATH, "//a[contains(@class,'global_action_link') and contains(@href, 'login')]")
 
-        wait = WebDriverWait(browser, 10)
+    LOGIN_PAGE_UNIQUE = (By.XPATH, "//div[contains(@class,'login_featuretarget_ctn')]")
+    LOGIN_INPUT_TEXT = (By.XPATH, "//div[contains(@class,'login_featuretarget_ctn')]//input[@type='text']")
+    LOGIN_INPUT_PASSWORD = (By.XPATH, "//div[contains(@class,'login_featuretarget_ctn')]//input[@type='password']")
+    LOGIN_SUBMIT_BUTTON = (By.XPATH, "//div[contains(@class,'login_featuretarget_ctn')]//button[@type='submit']")
+    LOADING_BUTTON_ELEMENT = (By.XPATH, "//button[@type='submit']/div/div")
+    ERROR_TEXT = (By.XPATH, "//div[contains(@class,'login_featuretarget_ctn')]//form/div[last()]")
 
-        assert "store.steampowered.com" in browser.current_url
-        wait.until(EC.title_contains("Steam"))
-        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.home_cluster_ctn")))
+@pytest.fixture
+def browser():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
 
-        btn_to_login = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "a.global_action_link[href*='/login/']")))
-        btn_to_login.click()
-        assert "login" in browser.current_url
-        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.login_featuretarget_ctn")))
+def test_login_with_invalid_credentials_shows_error(browser):
+    URL = "https://store.steampowered.com/"
+    WAIT = WebDriverWait(browser, 10)
+    fake = Faker()
 
-        login_input = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.login_featuretarget_ctn input[type='text']")))
-        password_input = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.login_featuretarget_ctn input[type='password']")))
-
-        login_input.send_keys(random_string())
-        password_input.send_keys(random_string())
-
-        btn_login = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.login_featuretarget_ctn button[type='submit']")))
-        btn_login.click()
-
-        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button[type='submit'] div div")))
-        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.login_featuretarget_ctn button[type='submit']")))
-
-        wait.until(EC.visibility_of_element_located((By.XPATH, "//div[contains(text(), 'Пожалуйста, проверьте свой пароль')]")))
+    #Navigate to main page
+    browser.get(URL)
+    WAIT.until(DocumentReady())
+    WAIT.until(EC.visibility_of_element_located(LOCATORS.MAIN_PAGE_UNIQUE))
+    WAIT.until(EC.visibility_of_element_located(LOCATORS.MAIN_PAGE_UNIQUE2))
+    #Click login button
+    WAIT.until(EC.visibility_of_element_located(LOCATORS.LOGIN_BUTTON)).click()
+    WAIT.until(DocumentReady())
+    WAIT.until(EC.visibility_of_element_located(LOCATORS.LOGIN_PAGE_UNIQUE))
+    #Input random strings as credentials. Click sign in button
+    WAIT.until(EC.visibility_of_element_located(LOCATORS.LOGIN_INPUT_TEXT)).send_keys(fake.text(8))
+    WAIT.until(EC.visibility_of_element_located(LOCATORS.LOGIN_INPUT_PASSWORD)).send_keys(fake.text(8))
+    WAIT.until(EC.visibility_of_element_located(LOCATORS.LOGIN_SUBMIT_BUTTON)).click()
+    WAIT.until(EC.visibility_of_element_located(LOCATORS.LOADING_BUTTON_ELEMENT))
+    WAIT.until(EC.visibility_of_element_located(LOCATORS.ERROR_TEXT))
