e2dd149275e83054163a5ea3a49bb0f88740446e
happyden <deaytthwt@gmail.com>
2026-02-02 16:12:40 +0600

    add assert for error text on invalid login

--- tests/test_steam_main_page.py
+++ tests/test_steam_main_page.py
@@ -13,9 +13,20 @@ class DocumentReady:
         except:
             return False
 
-class LOCATORS:
-    MAIN_PAGE_UNIQUE = (By.XPATH, "//div[contains(@class,'home_cluster_ctn') and contains(@class,'home_ctn')]")
-    MAIN_PAGE_UNIQUE2 = (By.XPATH, "//h2[@id='home_featured_and_recommended']")
+class ElementHasNonEmptyText:
+    def __init__(self, locator):
+        self.locator = locator
+
+    def __call__(self, driver):
+        try:
+            element = driver.find_element(*self.locator)
+            text = element.text.strip()
+            return text if text else False
+        except:
+            return False
+
+class Locators:
+    MAIN_PAGE_UNIQUE = (By.XPATH, "//h2[@id='home_featured_and_recommended']")
     LOGIN_BUTTON = (By.XPATH, "//a[contains(@class,'global_action_link') and contains(@href, 'login')]")
 
     LOGIN_PAGE_UNIQUE = (By.XPATH, "//div[contains(@class,'login_featuretarget_ctn')]")
@@ -23,7 +34,7 @@ class LOCATORS:
     LOGIN_INPUT_PASSWORD = (By.XPATH, "//div[contains(@class,'login_featuretarget_ctn')]//input[@type='password']")
     LOGIN_SUBMIT_BUTTON = (By.XPATH, "//div[contains(@class,'login_featuretarget_ctn')]//button[@type='submit']")
     LOADING_BUTTON_ELEMENT = (By.XPATH, "//button[@type='submit']/div/div")
-    ERROR_TEXT = (By.XPATH, "//div[contains(@class,'login_featuretarget_ctn')]//form/div[last()]")
+    ERROR_TEXT_ELEMENT = (By.XPATH, "//div[contains(@class,'login_featuretarget_ctn')]//form/div[last()]")
 
 @pytest.fixture
 def browser():
@@ -32,22 +43,26 @@ def browser():
     driver.quit()
 
 def test_login_with_invalid_credentials_shows_error(browser):
-    URL = "https://store.steampowered.com/"
-    WAIT = WebDriverWait(browser, 10)
+    timeout = 10
+    url = "https://store.steampowered.com/"
+    wait = WebDriverWait(browser, timeout)
     fake = Faker()
 
     #Navigate to main page
-    browser.get(URL)
-    WAIT.until(DocumentReady())
-    WAIT.until(EC.visibility_of_element_located(LOCATORS.MAIN_PAGE_UNIQUE))
-    WAIT.until(EC.visibility_of_element_located(LOCATORS.MAIN_PAGE_UNIQUE2))
+    browser.get(url)
+    wait.until(DocumentReady())
+    wait.until(EC.visibility_of_element_located(Locators.MAIN_PAGE_UNIQUE))
     #Click login button
-    WAIT.until(EC.visibility_of_element_located(LOCATORS.LOGIN_BUTTON)).click()
-    WAIT.until(DocumentReady())
-    WAIT.until(EC.visibility_of_element_located(LOCATORS.LOGIN_PAGE_UNIQUE))
+    wait.until(EC.visibility_of_element_located(Locators.LOGIN_BUTTON)).click()
+    wait.until(DocumentReady())
+    wait.until(EC.visibility_of_element_located(Locators.LOGIN_PAGE_UNIQUE))
     #Input random strings as credentials. Click sign in button
-    WAIT.until(EC.visibility_of_element_located(LOCATORS.LOGIN_INPUT_TEXT)).send_keys(fake.text(8))
-    WAIT.until(EC.visibility_of_element_located(LOCATORS.LOGIN_INPUT_PASSWORD)).send_keys(fake.text(8))
-    WAIT.until(EC.visibility_of_element_located(LOCATORS.LOGIN_SUBMIT_BUTTON)).click()
-    WAIT.until(EC.visibility_of_element_located(LOCATORS.LOADING_BUTTON_ELEMENT))
-    WAIT.until(EC.visibility_of_element_located(LOCATORS.ERROR_TEXT))
+    wait.until(EC.visibility_of_element_located(Locators.LOGIN_INPUT_TEXT)).send_keys(fake.text(8))
+    wait.until(EC.visibility_of_element_located(Locators.LOGIN_INPUT_PASSWORD)).send_keys(fake.text(8))
+    wait.until(EC.visibility_of_element_located(Locators.LOGIN_SUBMIT_BUTTON)).click()
+    wait.until(EC.visibility_of_element_located(Locators.LOADING_BUTTON_ELEMENT))
+    wait.until(EC.element_to_be_clickable(Locators.LOGIN_BUTTON))
+
+    actual_text = wait.until(ElementHasNonEmptyText(Locators.ERROR_TEXT_ELEMENT))
+    expected_text = "Пожалуйста, проверьте свой пароль и имя аккаунта и попробуйте снова."
+    assert expected_text in actual_text, f"Expected text not found in actual text: {actual_text}"
