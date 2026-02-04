331311985ce2cd2b077ad8624783db96f1f3c968
happyden <deaytthwt@gmail.com>
2026-02-04 16:28:38 +0600

    Browser singleton moved to utils/browser.py, removed from conftest.py

--- conftest.py
+++ conftest.py
@@ -1,28 +1,8 @@
 import pytest
-from selenium import webdriver
-
-class SingletonBrowser:
-    _instance = None
-    _driver = None
-
-    def __new__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super(SingletonBrowser, cls).__new__(cls)
-        return cls._instance
-
-    @property
-    def driver(self):
-        if self._driver is None:
-            self._driver = webdriver.Chrome()
-        return self._driver
-
-    def quit(self):
-        if self._driver:
-            self._driver.quit()
-            self._driver = None
+from utils.browser import  Browser
 
 @pytest.fixture(scope="session")
 def browser():
-    browser_instance = SingletonBrowser()
-    yield browser_instance.driver
-    browser_instance.quit()
+    driver = Browser()
+    yield driver
+    driver.quit()
